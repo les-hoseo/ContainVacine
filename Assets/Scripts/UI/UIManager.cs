070ff2a209e46dad914a533a1854b1065788444c
les-hoseo <20251129@vision.hoseo.edu>
2025-07-16 16:50:51 +0900

    2025-07-18_16:49
    +세이브 창, 세이브 나가기 버튼,세이브 버튼 프리펩

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    [Header ("설정 창")]
+    [Header("설정 창")]
     public GameObject settingPanel;
 
     [Header("게임 스타트")]
     public GameObject GameStart;
 
     [Header("인터페이스")]
-    public GameObject Interface; 
+    public GameObject Interface;
+
+    [Header("세이브창")]
+    public GameObject Save;
 
     // 세팅 UI
     // UI 열기
@@ -24,7 +27,7 @@ public class UIManager : MonoBehaviour
         settingPanel.SetActive(false);
     }
 
-//Game Start Button UI
+    //Game Start Button UI
     //UI 열기
     public void ShowGameStartbutton()
     {
@@ -46,5 +49,18 @@ public class UIManager : MonoBehaviour
     public void HideINTERFACE()
     {
         Interface.SetActive(false);
+    }
+
+    //Save UI
+    // UI 열기
+    public void ShowSaveUI()
+    {
+        Debug.Log("세팅창 켜볼게");
+        Save.SetActive(true);
+    }
+    // UI 닫기
+    public void HideSaveUI()
+    {
+        Save.SetActive(false);
     }
 }
--- Assets/Scripts/UI/speech.cs
+++ Assets/Scripts/UI/speech.cs
@@ -33,8 +33,7 @@ public class speech : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0) && !INTERFACE_BACKBROUND.activeSelf)
         {
-            Debug.Log(Input.mousePosition.x);
-            Debug.Log(Input.mousePosition.y);
+            
             if (Input.mousePosition.x > 0 && Input.mousePosition.x < 1920 && Input.mousePosition.y > 0 && Input.mousePosition.y < 350)
             {
                 if (Running)
